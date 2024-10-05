3a6e69780d32037f3f0cd1ecaa3171376c63821f
LyaSantos24 <147869869+LyaSantos24@users.noreply.github.com>
2024-10-04 21:46:43 -0400

    switch.java

---
+++ switch.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+      int opcoes =5;
+
+      switch (opcoes) {
+
+          case 1:
+              System.out.println("Abrir uma nova conta");
+              break;
+
+          case 2:
+              System.out.println("Abrir uma conta existente");
+              break;
+
+          case 3:
+              System.out.println("Credito Imobiliario");
+              break;
+
+          case 4:
+              System.out.println("Nenhuma opcao foi selecionada");
+              break;
+              default:
+      }
+
+   }
+
+}
